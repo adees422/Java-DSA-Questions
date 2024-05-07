35dd36881dfcb1a5c338691db28f91bae5794efe
adees422 <166525635+adees422@users.noreply.github.com>
2024-05-07 12:36:33 -0500

    Add files via upload

--- longestseq.java
+++ longestseq.java
@@ -1,37 +1,34 @@
 import java.util.*;
 class longestseq
 {
-public static boolean m1(int arr[],int num)
+public static int m1(int arr[])
 {
 int n =arr.length;
-for(int i=0;i<n;i++)
-{
-if(arr[i]==num)
-return true;
-}
-return false;
-}
-public static int m2(int arr[])
-{
-int n=arr.length;
+Arrays.sort(arr);
+
+int lastsmaller=Integer.MIN_VALUE;
 int longest=1;
+int cnt=0;
 for(int i=0;i<n;i++)
 {
-int x=arr[i];
-int cnt=1;
-while(m1(arr,x+1)==true)
+if(arr[i]-1==lastsmaller)
 {
-x+=1;
 cnt+=1;
+lastsmaller=arr[i];
 }
-longest =Math.max(longest,cnt);
+else if(arr[i]!=lastsmaller)
+{
+cnt=1;
+lastsmaller=arr[i];
+}
+longest=Math.max(longest,cnt);
 }
 return longest;
 }
-public static void main (String[]args)
+public static void main(String[]args)
 {
-int arr[]={200,100,1,2,3,4,5};
-int ans= m2(arr);
+int arr[]={100,200,1,2,3,4};
+int ans =m1(arr);
 System.out.print(ans);
-
-}}
+}
+}
