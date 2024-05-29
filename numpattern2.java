a8e135d4b4005517e185b44e63fad0beb28775fc
adees422 <166525635+adees422@users.noreply.github.com>
2024-05-29 03:45:13 -0500

    Add files via upload

---
+++ numpattern2.java
@@ -0,0 +1,18 @@
+class numpattern2
+{
+public static void main(String[]args)
+{
+int count=1;
+int i,j,rows=4;
+for(i=1;i<=6;i++)
+{
+for(j=1;j<=i;j++)
+{
+System.out.print(count + " ");
+count++;
+
+}
+System.out.println();
+}
+}
+}
